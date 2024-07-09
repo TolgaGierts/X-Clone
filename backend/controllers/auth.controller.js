3b6330fb5331d9fd4233372ca1721f8f77185ded
TolgaGierts <40353577+TolgaGierts@users.noreply.github.com>
2024-07-09 21:29:24 +0300

    project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const Signup = async (req, res) => {
+    res.json({ data: "signup" })
+}
+
+export const Login = async (req, res) => {
+    res.json({ data: "Login" })
+}
+
+export const Logout = async (req, res) => {
+    res.json({ data: "logout" })
+}
