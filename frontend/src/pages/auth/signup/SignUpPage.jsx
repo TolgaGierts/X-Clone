f0631a77162f73e4a1150047825ffdcaf30cbbb7
TolgaGierts <40353577+TolgaGierts@users.noreply.github.com>
2024-07-12 22:55:42 +0300

    sign up page invalidate query

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -7,7 +7,7 @@ import { MdOutlineMail } from 'react-icons/md';
 import { FaUser } from 'react-icons/fa';
 import { MdPassword } from 'react-icons/md';
 import { MdDriveFileRenameOutline } from 'react-icons/md';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 
 const SignUpPage = () => {
@@ -17,6 +17,7 @@ const SignUpPage = () => {
     fullName: '',
     password: '',
   });
+  const queryClient = useQueryClient();
 
   const { mutate, isError, isPending, error } = useMutation({
     mutationFn: async ({ email, username, fullName, password }) => {
@@ -40,6 +41,7 @@ const SignUpPage = () => {
     },
     onSuccess: () => {
       toast.success('User created successfully');
+      queryClient.invalidateQueries({ queryKey: ['authUser'] });
     },
     onError: (error) => {
       console.log(error);
