6e6731f802d3378b085a38c277ef1437178d307f
TolgaGierts <tolgaa@hotmail.fr>
2024-07-12 18:48:23 +0300

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 8000
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
@@ -28,6 +29,15 @@ app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
 app.use('/api/notifications', notificationRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    })
+}
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
